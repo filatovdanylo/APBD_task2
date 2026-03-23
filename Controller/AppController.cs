b81c55fb13eadaf2a17dc10a6d2f1f9f30476cc7
Danylo Filatov <s32793@pjwstk.edu.pl>
2026-03-23 23:11:59 +0200

    Added application controller that will process user commands inputed from the console

--- Controller/AppController.cs
+++ Controller/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using APBD_TASK2.Interface;
+using APBD_TASK2.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,34 @@ namespace APBD_TASK2.Controller
 {
     public class AppController
     {
-        
+
+        private static IRentalService _service = null!;
+        public static void Run(RentalService service)
+        {
+            _service = service;
+
+            while (true)
+            {
+                ShowMenu();
+                var choice = Console.ReadLine()?.Trim();
+                // do something
+            }
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("1.  Add user");
+            Console.WriteLine("2.  Add equipment");
+            Console.WriteLine("3.  List all equipment");
+            Console.WriteLine("4.  List available equipment");
+            Console.WriteLine("5.  Rent equipment");
+            Console.WriteLine("6.  Return equipment");
+            Console.WriteLine("7.  Mark equipment unavailable");
+            Console.WriteLine("8.  Active rentals for user");
+            Console.WriteLine("9.  Overdue rentals");
+            Console.WriteLine("10. Generate report");
+            Console.WriteLine("0.  Exit");
+            Console.WriteLine("Choose: ");
+        }
     }
 }
