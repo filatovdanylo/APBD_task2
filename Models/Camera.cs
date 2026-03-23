0bb6cbcf184e0fb57791c738132b312b5486bd0a
kugaru <fila.danila@gmail.com>
2026-03-23 20:57:33 +0200

    Basic models addded

---
+++ Models/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD_TASK2.Models
+{
+    public class Camera : Equipment
+    {
+        public int ResolutionMegapixels { get; set; }
+        public int WeightGrams { get; set; }
+        public bool CanShootVideo { get; set; }
+
+        public Camera(string name, int resolutionMegapixels, int weightGrams, bool canShootVideo) 
+            : base(name)
+        {
+            ResolutionMegapixels = resolutionMegapixels;
+            WeightGrams = weightGrams;
+            CanShootVideo = canShootVideo;
+        }
+    }
+}
---
+++ Models/Projector.cs
@@ -0,0 +1,23 @@
+﻿using APBD_TASK2.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD_TASK2.Models
+{
+    public class Projector : Equipment
+    {
+        public Resolution Resolution { get; set; }
+
+        public int BrightnessLumens { get; set; }
+
+        public Projector(string name, Resolution resolution, int brightnessLumens) 
+            : base(name)
+        {
+            Resolution = resolution;
+            BrightnessLumens = brightnessLumens;
+        }
+    }
+}
